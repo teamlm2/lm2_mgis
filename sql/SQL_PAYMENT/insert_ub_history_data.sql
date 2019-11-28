beca74b9403d2ad475f653aa91d58e05c27db978
B.Ankhbold <aagii_csms@yahoo.com>
2019-11-28 09:10:07 +0800

    2019-11-28

--- sql/SQL_PAYMENT/insert_ub_history_data.sql
+++ sql/SQL_PAYMENT/insert_ub_history_data.sql
@@ -1,6 +1,6 @@
 ﻿insert into data_ub.ub_fee_history(person_register, pid, contract_no, document_area, zoriulalt, ner, payment_contract, payment_before_less, payment_before_over, payment_year, payment_fund, payment_loss, payment_total,
-paid_year, paid_fund, paid_city, paid_district, invalid_payment, paid_less, paid_over, description, decision, current_year)
+paid_year, paid_fund, paid_city, paid_district, invalid_payment, paid_less, paid_over, description, decision, current_year, city_type, au2)
 
 select person_register, pid, contract_no, document_area, zoriulalt, ner, payment_contract, payment_before_less, payment_before_over, payment_year, payment_fund, payment_loss, payment_total,
-paid_year, paid_fund, paid_city, paid_district, invalid_payment, paid_less, paid_over, description, decision, current_year from data_ub.data_fee
-where person_register is not null
+paid_year, paid_fund, case when paid_city ~ '^([0-9]+[.]?[0-9]*|[.][0-9]+)$' then paid_city::numeric else 0 end as paid_city, paid_district, invalid_payment, paid_less, paid_over, description, decision, current_year, city_type, '01113' from data_ub.fee_duureg_nalaikh
+--where person_register is not null
