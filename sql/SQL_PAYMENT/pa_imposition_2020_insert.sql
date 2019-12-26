1004ccc8cbc52d77f7ecc11e5b096ffd2674abe0
B.Ankhbold <aagii_csms@yahoo.com>
2019-12-26 12:54:47 +0800

    2019-12-26 payment sql

--- sql/SQL_PAYMENT/pa_imposition_2020_insert.sql
+++ sql/SQL_PAYMENT/pa_imposition_2020_insert.sql
@@ -1,6 +1,6 @@
 ﻿
 -------------shalgah
-select * from data_estimate.paid_bayannuur_2019
+select * from data_estimate.uvs_paid_2019
 where contract_no not in
 (
 select contract_no from (
@@ -8,12 +8,12 @@ select row_number() over(partition by bayan.contract_no) as rank, bayan.* from d
 join data_soums_union.ct_contract c on im.contract_id = c.contract_id
 join data_soums_union.ct_fee fee on fee.id = im.fee_id
 join base.bs_person person on fee.person = person.person_id
-join data_estimate.paid_bayannuur_2019 bayan on im.parcel_id = bayan.parcel_id and upper(c.contract_no) = upper(bayan.contract_no) and upper(person.person_register) = upper(bayan.person_register)
+join data_estimate.uvs_paid_2019 bayan on im.parcel_id = bayan.parcel_id and upper(c.contract_no) = upper(bayan.contract_no) and upper(person.person_register) = upper(bayan.person_register)
 where im.imposition_year = 2019
 )xxx where rank = 1)
 
 
-select * from data_estimate.paid_bayannuur_2019
+select * from data_estimate.uvs_paid_2019
 
 --------------iluu dutuu tulultiin update hiih
 with new_numbers as (
@@ -21,7 +21,7 @@ select bayan.remaining_amount, im.id from data_estimate.pa_imposition im
 join data_soums_union.ct_contract c on im.contract_id = c.contract_id
 join data_soums_union.ct_fee fee on fee.id = im.fee_id
 join base.bs_person person on fee.person = person.person_id
-join data_estimate.paid_bayannuur_2019 bayan on im.parcel_id = bayan.parcel_id and upper(c.contract_no) = upper(bayan.contract_no) and upper(person.person_register) = upper(bayan.person_register)
+join data_estimate.uvs_paid_2019 bayan on im.parcel_id = bayan.parcel_id and upper(c.contract_no) = upper(bayan.contract_no) and upper(person.person_register) = upper(bayan.person_register)
 where im.imposition_year = 2019 and bayan.remaining_amount is not null
 )
 update data_estimate.pa_imposition set remainning_amount = nn.remaining_amount
@@ -39,9 +39,9 @@ where au2 = '06307' and imposition_year = 2019
 -----------------tulult hiih
 insert into data_estimate.pa_payment_paid(fee_id, imposition_id, paid_year, remainning_amount, quarter_four, total_amount, year_amount, au2, parcel_id, contract_id, person_id, contract_amount, imposition_year_amount, imposition_total_amount, type_id)
 
-select fee.id, im.id, 2019, abs(bayan.remaining_amount), bayan.paid_amount+bayan.remaining_amount, bayan.paid_amount, im.total_amount-bayan.paid_amount, '06307', bayan.parcel_id, c.contract_id, person.person_id, fee.fee_calculated, im.year_amount, im.total_amount, 1 from data_estimate.pa_imposition im
+select fee.id, im.id, 2019, abs(bayan.remaining_amount::numeric), bayan.paid_amount+bayan.remaining_amount::numeric, bayan.paid_amount, im.total_amount::numeric-bayan.paid_amount::numeric, '06307', bayan.parcel_id, c.contract_id, person.person_id, fee.fee_calculated, im.year_amount, im.total_amount, 1 from data_estimate.pa_imposition im
 join data_soums_union.ct_contract c on im.contract_id = c.contract_id
 join data_soums_union.ct_fee fee on fee.id = im.fee_id
 join base.bs_person person on fee.person = person.person_id
-join data_estimate.paid_bayannuur_2019 bayan on im.parcel_id = bayan.parcel_id and upper(c.contract_no) = upper(bayan.contract_no) and upper(person.person_register) = upper(bayan.person_register)
+join data_estimate.uvs_paid_2019 bayan on im.parcel_id = bayan.parcel_id and upper(c.contract_no) = upper(bayan.contract_no) and upper(person.person_register) = upper(bayan.person_register)
 where im.imposition_year = 2019 and bayan.paid_amount is not null
