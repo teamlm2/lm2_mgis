24e666198bc7b6080ac8d790fff9ad6b05c45e48
B.Ankhbold <aagii_csms@yahoo.com>
2020-03-26 18:23:53 +0800

    2020-03-26 ankhaa

---
+++ sql/SQL_PASTURE/service_point_full.sql
@@ -0,0 +1,20 @@
+﻿select
+  au1.code, au1.name, au2.code, au2.name, au3.code, au3.name,
+  pb.code, pb.group_name, pb.area_ga,
+  pp.parcel_id, pp.area_ga, pp.pasture_type,
+  d.*,
+  t.*
+  --point_detail_id, monitoring_year, register_date, area_ga, duration, rc, rc_precent, sheep_unit, sheep_unit_plant,
+  --biomass, d1, d1_100ga, d2, d3, d3_rc, unelgee, rc_id, begin_month, end_month
+from pasture.ps_point_d_value as t
+join pasture.ps_point_detail d on t.point_detail_id = d.point_detail_id
+
+join pasture.ps_point_detail_points dp on d.point_detail_id = dp.point_detail_id
+join pasture.ca_pasture_monitoring p on dp.point_id = p.point_id
+join data_soums_union.ca_pug_boundary pb on st_within(p.geometry, pb.geometry)
+join data_soums_union.ca_pasture_parcel_tbl pp on st_within(p.geometry, pp.geometry)
+join admin_units.au_level2 au2 on st_within(p.geometry, au2.geometry)
+join admin_units.au_level1 au1 on st_within(p.geometry, au1.geometry)
+join admin_units.au_level3 au3 on st_within(p.geometry, au3.geometry)
+where pp.group_type = 1 and t.monitoring_year = 2019 and au2.code = '06404'
+      and (LOWER(pp.pasture_type) ilike '%өвөл%' or LOWER(pp.pasture_type) ilike '%хавар%')
---
+++ sql/SQL_PASTURE/table_rc_change.sql
@@ -0,0 +1,47 @@
+﻿SET SEARCH_PATH TO pasture, admin_units, public;
+
+drop table if exists cl_rc_change;
+CREATE TABLE cl_rc_change
+(
+  code int NOT NULL primary key,
+  description character varying(500) NOT NULL,
+  description2 character varying(500) NOT NULL
+);
+grant select, insert, update, delete on cl_rc_change to address_admin;
+grant select on cl_rc_change to address_view;
+COMMENT ON TABLE cl_rc_change
+  IS 'Бэлчээрийн өөрчлөлтийн жагсаалт';
+
+insert into pasture.cl_rc_change values (1, '1.Зохистой ашиглалт бүхий бэлчээр', '1.Соргог бэлчээр');
+insert into pasture.cl_rc_change values (2, '2.Зохистой ашиглалтаар сайжруулж буй бэлчээр', '2.Сайжирч буй бэлчээр');
+insert into pasture.cl_rc_change values (3, '3.Ашиглалтын хэлбэрийг өөрчлөх шаардлагатай бэлчээр', '3.Доройтож буй бэлчээр');
+
+drop table if exists set_rc_change;
+CREATE TABLE set_rc_change
+(
+  id serial primary key,
+  rc_change_id int references cl_rc_change on update cascade on delete restrict,
+  value character varying(500) NOT NULL
+);
+grant select, insert, update, delete on set_rc_change to address_admin;
+grant select on set_rc_change to address_view;
+COMMENT ON TABLE set_rc_change
+  IS 'Бэлчээрийн өөрчлөлтийн тохиргоо';
+
+insert into pasture.set_rc_change values (1,  1, '1-1');
+insert into pasture.set_rc_change values (2,  1, '1-2');
+insert into pasture.set_rc_change values (3,  1, '2-1');
+insert into pasture.set_rc_change values (4,  1, '2-2');
+insert into pasture.set_rc_change values (5,  1, '3-1');
+insert into pasture.set_rc_change values (6,  1, '4-1');
+
+insert into pasture.set_rc_change values (7, 2, '3-2');
+insert into pasture.set_rc_change values (8, 2, '3-3');
+insert into pasture.set_rc_change values (9, 2, '4-2');
+insert into pasture.set_rc_change values (10, 2, '4-3');
+
+insert into pasture.set_rc_change values (11, 3, '1-3');
+insert into pasture.set_rc_change values (12, 3, '1-4');
+insert into pasture.set_rc_change values (13, 3, '2-3');
+insert into pasture.set_rc_change values (14, 3, '3-4');
+insert into pasture.set_rc_change values (15, 3, '4-4');
