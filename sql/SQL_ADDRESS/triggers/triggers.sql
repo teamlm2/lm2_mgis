2aa425805bd6c8b8ef742594cc9db80534f4830a
B.Ankhbold <aagii_csms@yahoo.com>
2020-05-07 12:00:41 +0800

    2020-05-07 ankhaa

---
+++ sql/SQL_ADDRESS/triggers/triggers.sql
@@ -0,0 +1,35 @@
+﻿CREATE TRIGGER update_area
+  BEFORE INSERT OR UPDATE
+  ON data_address.st_street
+  FOR EACH ROW
+  EXECUTE PROCEDURE base.update_area_or_length();
+
+CREATE TRIGGER update_area
+  BEFORE INSERT OR UPDATE
+  ON data_address.st_street_sub
+  FOR EACH ROW
+  EXECUTE PROCEDURE base.update_area_or_length();
+
+CREATE TRIGGER set_default_au1
+  BEFORE INSERT OR UPDATE
+  ON data_address.st_street
+  FOR EACH ROW
+  EXECUTE PROCEDURE base.set_default_au1();
+
+CREATE TRIGGER set_default_au2
+  BEFORE INSERT OR UPDATE
+  ON data_address.st_street
+  FOR EACH ROW
+  EXECUTE PROCEDURE base.set_default_au2();
+
+CREATE TRIGGER set_default_au1
+  BEFORE INSERT OR UPDATE
+  ON data_address.st_street_sub
+  FOR EACH ROW
+  EXECUTE PROCEDURE base.set_default_au1();
+
+CREATE TRIGGER set_default_au2
+  BEFORE INSERT OR UPDATE
+  ON data_address.st_street_sub
+  FOR EACH ROW
+  EXECUTE PROCEDURE base.set_default_au3();
