1167797ae05bf28ef7ef12d5a6ede250123ddedf
B.Ankhbold <aagii_csms@yahoo.com>
2020-05-13 18:28:08 +0800

    2020-04-02 ankhaa

---
+++ sql/SQL_GNS/1_postgres_create_db.sql
@@ -0,0 +1,3 @@
+﻿create database lm_gns template=template_postgis;
+
+grant create on database lm_gns to gns_admin;
---
+++ sql/SQL_GNS/3a_postgres_alter_tables.sql
@@ -0,0 +1,5 @@
+﻿GRANT ALL ON TABLE admin_units.au_level1 TO gns_admin;
+GRANT ALL ON TABLE admin_units.au_level2 TO gns_admin;
+GRANT ALL ON TABLE admin_units.au_level3 TO gns_admin;
+GRANT ALL ON TABLE admin_units.au_level4 TO gns_admin;
+GRANT ALL ON TABLE codelists.cl_landuse_type TO gns_admin;
