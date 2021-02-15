64c5aab51b8e11ca75628270f1781a523004b9c2
B.Ankhbold <aagii_csms@yahoo.com>
2021-02-16 00:40:20 +0800

    2021-01-15

--- sql/SQL_ADDRESS/convert/convert_entrance.sql
+++ sql/SQL_ADDRESS/convert/convert_entrance.sql
@@ -1,6 +1,10 @@
-﻿insert into data_address.st_entrance(type, geometry)
-select 1, geom from data_address_cd.bzd_entry
+﻿alter table data_address.st_entrance disable trigger a_create_address_entry_id;
 
-where gid >5000 and gid <= 10000
+insert into data_address.st_entrance(entrance_id, type, geometry)
+select 157288+row_number() over(), 1, geom from data_address_cd.bzd_entry
+
+where gid >20000 and gid <= 25000
 
 ---and geom in (select geometry from data_address.st_entrance)
+
+SELECT max(entrance_id::int) FROM data_address.st_entrance
