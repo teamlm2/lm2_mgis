d2aa2e7f092db8d56dd756eece309ecb0436d6fd
B.Ankhbold <aagii_csms@yahoo.com>
2021-05-27 19:19:05 +0800

    2021-05-27 office

---
+++ sql/SQL_ADDRESS/convert/convert_cadastre_building.sql
@@ -0,0 +1,7 @@
+﻿insert into data_address.ca_building_address_history(building_id, is_active, in_source, zipcode_id, street_id, address_building_no, address_parcel_no au1, au2, created_at, valid_from, valid_till, geometry)
+
+select xxx.building_id, true, 1, null, null, xxx.building_no, xxx.address_khashaa, substring(xxx.au2, 1, 3), xxx.au2, now(), xxx.valid_from, xxx.valid_till, xxx.geometry from (
+select aa.building_id, cba.building_id as address_building_id, aa.building_no, aa.address_khashaa, aa.au2, aa.valid_from, aa.valid_till, aa.geometry from data_soums_union.ca_building_tbl aa 
+left join data_address.ca_building_address cba on aa.building_id = cba.building_id 
+where substring(aa.au2, 1, 3) = '081'
+)xxx where address_building_id is null
--- sql/SQL_ADDRESS/convert/convert_cadastre_parcel.sql
+++ sql/SQL_ADDRESS/convert/convert_cadastre_parcel.sql
@@ -2,9 +2,23 @@
 status, parcel_type, geometry, valid_from, valid_till)
 
 select p.parcel_id, true, 1, null, null, address_khashaa, address_streetname, address_neighbourhood, null, substring(au2, 1, 3), au2, au3, 1, 1, 1, geometry, valid_from, valid_till from data_soums_union.ca_parcel_tbl p
-where now() between p.valid_from and p.valid_till and p.parcel_id not in ( select parcel_id from data_address.ca_parcel_address p where substring(p.au2, 1, 3) = '021') and substring(p.au2, 1, 3) = '021'
+where now() between p.valid_from and p.valid_till and p.parcel_id not in ( select parcel_id from data_address.ca_parcel_address pp where substring(pp.au2, 1, 3) = '081') and substring(p.au2, 1, 3) = '081'
+
+
+select p.parcel_id, true, 1, null, null, address_khashaa, address_streetname, address_neighbourhood, null, substring(au2, 1, 3), au2, au3, 1, 1, 1, geometry, valid_from, valid_till from data_soums_union.ca_parcel_tbl p
+where now() between p.valid_from and p.valid_till and p.parcel_id not in ( select parcel_id from data_address.ca_parcel_address pp where pp.au2 = '08110') and p.au2 = '08110'
 --ON CONFLICT (parcel_id) DO NOTHING;
 
+--барилгын сууриар нэгж талбар болгож оруулах
+
+insert into data_address.ca_parcel_address(parcel_id, is_active, in_source, street_id, address_parcel_no, address_streetname, au1, au2, au3, 
+status, parcel_type, geometry, valid_from, valid_till, created_at)
+
+select aa.id, true, 2, street_id, address_parcel_no, address_streetname, substring(au2, 1, 3), au2, au3, 1, 12, aa.geometry, valid_from, valid_till, now() from data_address.ca_building_address aa 
+where aa.au2 = '08110' and aa.id not in (select cba.id from data_address.ca_building_address cba 
+join data_address.ca_parcel_address cpa on st_overlaps(cpa.geometry, cba.geometry) or st_covers(cpa.geometry, cba.geometry)
+where cba.au2 = '08110' and cpa.au2 = '08110' group by cba.id
+)group by aa.id
 
 
 ------history
